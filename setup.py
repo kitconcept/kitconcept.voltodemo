430d153edb65ab6738e9fddae8f7b0093e3b7638
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-02-12 23:27:28 +0100

    Preparing release 1.0.0a5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='kitconcept.voltodemo',
-    version='1.0.0a5.dev0',
+    version='1.0.0a5',
     description='',
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
