310639f0afe01b972f04007ac8d7c2d5d6cde75d
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-10-31 10:08:40 +0100

    Fix syntax error

--- src/kitconcept/voltodemo/interfaces.py
+++ src/kitconcept/voltodemo/interfaces.py
@@ -4,6 +4,8 @@ from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 from plone.supermodel import model
 from zope import schema
 from zope.interface import Interface
+
+
 class IKitconceptvoltodemoCoreLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
 
@@ -12,12 +14,13 @@ class IHomepage(model.Schema):
     """ Homepage content type interface
     """
 
+
 class IVoltoSettings(Interface):
     """ Volto settings necessary to store ont he backend
     """
 
     frontend_domain = schema.URI(
-        title = "Frontend domain",
-        description = "Used for rewriting URL's sent in the password reset e-mail by Plone." \
-        default="http://localhost:3000"
+        title="Frontend domain",
+        description="Used for rewriting URL's sent in the password reset e-mail by Plone.",
+        default="http://localhost:3000",
     )
